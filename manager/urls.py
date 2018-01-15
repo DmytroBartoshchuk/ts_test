6d4f8e151e9d433ce054825afcb88fd0b05f56e6
bartdm <dmitriy.bartoshchuk@gmail.com>
2018-01-16 00:38:57 +0100

    dynamic urls

--- manager/urls.py
+++ manager/urls.py
@@ -8,18 +8,18 @@ urlpatterns = [
     # /manager/carriers/
     url(r'^carriers/$', views.carrier_list, name='carrier_list'),
 
-    # /manager/carriers/?/
+    # /manager/carriers/<carrier_id>/
     url(r'^carriers/(?P<carrier_id>[0-9]+)/$', views.carrier_detail, name='carrier_detail'),
 
     # /manager/orders/
     url(r'^orders/$', views.order_list, name='order_list'),
 
-    # /manager/orders/?/
+    # /manager/orders/<order_id>/
     url(r'^orders/(?P<order_id>[0-9]+)/$', views.order_detail, name='order_detail'),
 
     # /manager/trips/
     url(r'^trips/$', views.trip_list, name='trip_list'),
 
-    # /manager/trip/?/
+    # /manager/trip/<trip_id>/
     url(r'^trips/(?P<trip_id>[0-9]+)/$', views.trip_detail, name='trip_detail'),
 ]
