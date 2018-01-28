d6d04f2772df26c60c47ccb5acc1e111ee449b8d
bartdm <dmitriy.bartoshchuk@gmail.com>
2018-01-28 19:57:26 +0100

    custom admin

--- manager/admin.py
+++ manager/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Carrier, Order, Trip
 
+
 admin.site.register(Carrier)
 admin.site.register(Trip)
 admin.site.register(Order)
+
+admin.site.site_header = 'TickSell administration'
+
+
